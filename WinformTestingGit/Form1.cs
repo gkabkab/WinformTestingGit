337a7d50e4723a3d35a4696b267d133255b12eff
Georges Kabkab <gkabkab@hotmail.com>
2021-07-02 13:43:05 +0300

    add button

--- WinformTestingGit/Form1.cs
+++ WinformTestingGit/Form1.cs
@@ -16,5 +16,10 @@ namespace WinformTestingGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
