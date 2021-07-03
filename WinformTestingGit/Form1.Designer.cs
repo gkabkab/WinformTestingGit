b10b222453200e4934d90a6ec9cfca6362bab064
Georges Kabkab <gkabkab@hotmail.com>
2021-07-03 15:51:38 +0300

    textbox

--- WinformTestingGit/Form1.Designer.cs
+++ WinformTestingGit/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace WinformTestingGit
             // 
             this.button1.Location = new System.Drawing.Point(56, 90);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(224, 136);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
