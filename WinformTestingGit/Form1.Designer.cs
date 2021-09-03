7edda0eeb2028566537862a5cbef91c1c9fb2957
Georges Kabkab <gkabkab@hotmail.com>
2021-09-03 11:35:46 +0300

    GK B1 Added B5

--- WinformTestingGit/Form1.Designer.cs
+++ WinformTestingGit/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace WinformTestingGit
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -80,11 +81,21 @@ namespace WinformTestingGit
             this.button4.Text = "Georges kabkab";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(537, 249);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(224, 136);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Georges kabkab";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -104,6 +115,7 @@ namespace WinformTestingGit
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
