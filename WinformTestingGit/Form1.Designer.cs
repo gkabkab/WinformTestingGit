cb3d987298d1ce4d48b66151e49e118f0813dcbd
Georges Kabkab <gkabkab@hotmail.com>
2021-09-03 11:34:02 +0300

    GK B1 Added Fouth Button

--- WinformTestingGit/Form1.Designer.cs
+++ WinformTestingGit/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace WinformTestingGit
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -70,11 +71,21 @@ namespace WinformTestingGit
             this.button3.Text = "Georges kabkab";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(296, 249);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 136);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Georges kabkab";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -92,6 +103,7 @@ namespace WinformTestingGit
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
