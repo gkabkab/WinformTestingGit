1e0d84a6619963f0067ae563d79e4cd1b6ffba4f
Georges Kabkab <gkabkab@hotmail.com>
2021-09-03 11:31:27 +0300

    GK B1 Add third Button

--- WinformTestingGit/Form1.Designer.cs
+++ WinformTestingGit/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace WinformTestingGit
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -60,11 +61,21 @@ namespace WinformTestingGit
             this.button2.Text = "Georges kabkab";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 136);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Georges kabkab";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -80,6 +91,7 @@ namespace WinformTestingGit
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
